da0c520cd6326ce141f7a07106b048af5c6749d3
Matthew Star <your.mattjstar@gmail.com>
2013-08-22 14:49:06 -0400

    Use a dynamic secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -8,5 +8,21 @@
 # You can use `rake secret` to generate a secure secret key.
 
 # Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-SampleApp40::Application.config.secret_key_base = '12dd8f28b817d0d1e104f29566620897c96b4d5de41147793892cc9f4855c66c1b04a950858e676f5261a1bd33dbe63df312e272fa88cc87194feef8840e8652'
+# if you're sharing your code publicly. such as by adding
+# .secret to your .gitignore file.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end 
+end
+
+SampleApp40::Application.config.secret_key_base = secure_token
